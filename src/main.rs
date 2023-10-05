95b0c7cf482a44ef7683f5de5899125cbe5ea89c
jjtt <3908945+jjtt@users.noreply.github.com>
2023-10-05 20:13:20 +0300

    finnish

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,74 @@
 use itertools::Itertools;
+use std::iter;
+use std::ops::Range;
 
 fn main() {
-    let combinations = (0..16).permutations(7);
+    let count = 7;
+    let func = sentence;
+    let combinations = (1..=count).map(|_| 0..16).multi_cartesian_product();
     combinations
         .map(|c| {
-            let s = sentence(&c);
+            let s = func(&c);
             (c, s)
         })
-        .map(|(c, s)| (c, hash_half_bytes_as_vec(&s)))
+        .map(|(c, s)| (c, hash_half_bytes_as_vec(&s, count)))
         .filter(|(c, s)| c == s)
-        .for_each(|(c, s)| println!("{}", sentence(&c)));
+        .for_each(|(c, s)| println!("{}", func(&c)));
 }
 
 /// a const array of 16 strings
-const FOO: [&str; 16] = [
+const ENGLISH: [&str; 16] = [
     "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "a", "b", "c",
     "d", "e", "f",
+];
+/// a const array of 16 strings
+const FINNISH: [&str; 16] = [
+    "nolla",
+    "yksi",
+    "kaksi",
+    "kolme",
+    "neljä",
+    "viisi",
+    "kuusi",
+    "seitsemän",
+    "kahdeksan",
+    "yhdeksän",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
 ];
 
 fn sentence(input: &Vec<usize>) -> String {
     format!(
         "The SHA256 for this sentence begins with: {}, {}, {}, {}, {}, {} and {}.",
-        FOO[input[0]],
-        FOO[input[1]],
-        FOO[input[2]],
-        FOO[input[3]],
-        FOO[input[4]],
-        FOO[input[5]],
-        FOO[input[6]],
+        ENGLISH[input[0]],
+        ENGLISH[input[1]],
+        ENGLISH[input[2]],
+        ENGLISH[input[3]],
+        ENGLISH[input[4]],
+        ENGLISH[input[5]],
+        ENGLISH[input[6]],
+    )
+}
+fn lause(input: &Vec<usize>) -> String {
+    format!(
+        "Tämän lauseen SHA256 alkaa: {}, {}, {}, {}, {}, {}, {}, {}.",
+        FINNISH[input[0]],
+        FINNISH[input[1]],
+        FINNISH[input[2]],
+        FINNISH[input[3]],
+        FINNISH[input[4]],
+        FINNISH[input[5]],
+        FINNISH[input[6]],
+        FINNISH[input[7]],
     )
 }
 
-/// return first 7 chars from sha256 hash of input
-fn hash(input: &str) -> String {
-    use sha2::{Digest, Sha256};
-    let mut hasher = Sha256::new();
-    hasher.update(input);
-    let result = hasher.finalize();
-    let result = format!("{:x}", result);
-    result[..7].to_string()
-}
-
 /// return first 7 half bytes from sha256 hash of input
-fn hash_half_bytes_as_vec(input: &str) -> Vec<usize> {
+fn hash_half_bytes_as_vec(input: &str, i: usize) -> Vec<usize> {
     use sha2::{Digest, Sha256};
     let mut hasher = Sha256::new();
     hasher.update(input);
@@ -50,7 +76,7 @@ fn hash_half_bytes_as_vec(input: &str) -> Vec<usize> {
     let result = format!("{:x}", result);
     result
         .chars()
-        .take(7)
+        .take(i)
         .map(|c| c.to_digit(16).unwrap() as usize)
         .collect()
 }
